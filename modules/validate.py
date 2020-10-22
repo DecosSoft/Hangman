ef8b307dd60190abe83934afca41a156f5dc9d46
DecosSoft <58489850+DecosSoft@users.noreply.github.com>
2020-10-22 15:57:55 -0500

    Update validate.py

--- modules/validate.py
+++ modules/validate.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+"""
++ FileName  =  validate.py
++  Created  =  30/07/2005
++   Author  =  DecosSoft
++  Version  =  1.0
+"""
+
 from modules import efects, tries, styles
 from modules.language import language
 from time import sleep
