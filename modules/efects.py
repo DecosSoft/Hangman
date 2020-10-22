ba707d41a1fb9a1ecb4b9927b120007eae42914f
DecosSoft <58489850+DecosSoft@users.noreply.github.com>
2020-10-22 15:48:22 -0500

    Update efects.py

--- modules/efects.py
+++ modules/efects.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+"""
++ FileName  =  efects.py
++  Created  =  30/07/2005
++   Author  =  DecosSoft
++  Version  =  1.0
+"""
+
 from sys import stdout
 from time import sleep
 
-def Writer(s,z):
+def Writer(string,z):
     sleep(0.3)
-    for i in s + '\n':
+    for i in string + '\n':
         stdout.write(i)
         stdout.flush()
         sleep(z / 100)
