9382453df45c62d38e6be435775cabd5c84a1011
DecosSoft <58489850+DecosSoft@users.noreply.github.com>
2020-10-22 15:45:21 -0500

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
 """
-+ FileName  =  banner.py
++ FileName  =  hangman.py
 +  Created  =  30/07/2005
 +   Author  =  DecosSoft
 +  Version  =  1.0
